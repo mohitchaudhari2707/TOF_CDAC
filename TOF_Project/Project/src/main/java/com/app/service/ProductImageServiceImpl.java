072540020aa883bbf487e628e9d2275531169faa
mohitchaudhari2707 <mohitchaudhari2707@gmail.com>
2022-04-03 17:19:20 +0530

    Added project

---
+++ TOF_Project/Project/src/main/java/com/app/service/ProductImageServiceImpl.java
@@ -0,0 +1,36 @@
+package com.app.service;
+
+import java.io.IOException;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.app.dao.ProductImageRepository;
+import com.app.pojos.ProductImage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+@Transactional
+public class ProductImageServiceImpl implements IProductImageService {
+	@Autowired
+	private ProductImageRepository imgRepo;
+	
+	@Override
+	public ProductImage findByProductName(String pName, MultipartFile imageFile) {
+		try {
+			ProductImage i = new ObjectMapper().readValue(pName, ProductImage.class);
+			i.setImage(imageFile.getBytes());
+			i.setImageContentType(imageFile.getContentType());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public ProductImage getImageByName(String productName) {
+		return imgRepo.findByProductName(productName);
+	}
+}
